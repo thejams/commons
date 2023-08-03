57ae1905155e9743d5b7dfb757095129ba3e7fcd
Jose Montero <thejams@MacBook-Pro-de-Baiser.local>
2023-08-03 17:27:01 -0400

    feat: refactor in metrics package

--- metrics/metrics_datadog.go
+++ metrics/metrics_datadog.go
@@ -3,6 +3,6 @@ package metrics
 import "github.com/DataDog/datadog-go/statsd"
 
 // NewDDClient returns a new datadog client
-func NewDDClient(host string) (Metrics, error) {
+func NewDDClient(host string) (MetricsClient, error) {
 	return statsd.New(host)
 }
